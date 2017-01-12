c9f3f01ed5fdc1b79af49b202dde3dd49abbdfd4
Dmitriy Berkutov <sodrian@gmail.com>
2017-01-12 10:11:06 +0000

    Simple Pig Latin

---
+++ kt28_simple_pig_latin.py
@@ -0,0 +1,6 @@
+import re
+WORD_PTRN = r'^\w+$'
+
+def pig_it(text):
+    print(text)
+    return ' '.join(['{0}{1}{2}'.format(word[1:], word[0], 'ay') if re.match(WORD_PTRN, word) else word for word in text.split()])
