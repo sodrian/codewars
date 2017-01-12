b4e4d73eac0cfd1bfec8fc828b62177f98cdd51c
Dmitriy Berkutov <sodrian@gmail.com>
2017-01-12 11:02:35 +0000

    editing kt24_your_order_please.py

--- kt24_your_order_please.py
+++ kt24_your_order_please.py
@@ -3,12 +3,10 @@ PTRN = r'[a-zA-Z]*(\d+)[a-zA-Z]*'
 
 
 def order(sentence):
-    print(sentence)
     ret = list()
     for el in sentence.split(' '):
         if el:
             d = re.findall(PTRN, el)[0]
             ret.append([d, el])
-    print(ret)
     ret = sorted(ret, key=lambda el: el[0])
     return ' '.join([el[1] for el in ret])
