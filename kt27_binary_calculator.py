77b9b284ad792307b9537c365caca4b25b6d0dcd
Dmitriy Berkutov <sodrian@gmail.com>
2017-01-12 11:02:35 +0000

    editing kt27_binary_calculator.py

--- kt27_binary_calculator.py
+++ kt27_binary_calculator.py
@@ -1,14 +1,10 @@
 import re
+from operator import add, sub, mul
 BIN_PTRN = r'([-]{0,1})0b(\d+)'
 
 def calculate(n1, n2, o):
-    add = lambda x, y: x + y
-    subtract = lambda x, y: x - y
-    multiply = lambda x, y: x * y 
-    divide = lambda x, y: x / y
     f = {
         'add': add,
-        'subtract': subtract,
-        'multiply': multiply,
-        'divide': divide}
+        'subtract': sub,
+        'multiply': mul}
     return ''.join(re.findall(BIN_PTRN, bin(f[o](int(n1, 2), int(n2, 2))))[0])
