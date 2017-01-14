967b140544650785ecce278175a0ab8d4d4383dc
Dmitriy Berkutov <sodrian@gmail.com>
2017-01-14 10:06:41 +0300

    editing kt29_simple_pig_latin.py

--- kt29_simple_pig_latin.py
+++ kt29_simple_pig_latin.py
@@ -1,3 +1,23 @@
+"""Title: Simple Pig Latin
+URL: https://www.codewars.com/kata/simple-pig-latin
+Description:
+
+Move the first letter of each word to the end of it, then add 'ay' to the end of the word.
+
+```javascript
+pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+```
+```ruby
+pig_it('Pig latin is cool') # igPay atinlay siay oolcay
+```
+```python
+pig_it('Pig latin is cool') # igPay atinlay siay oolcay
+```
+```csharp
+Kata.PigIt("Pig latin is cool"); // igPay atinlay siay oolcay
+```
+"""
+
 import re
 WORD_PTRN = r'^[\w]+$'
 
