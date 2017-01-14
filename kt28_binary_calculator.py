1314f080333ee5f3032575dd14f2e63bc8f8e591
Dmitriy Berkutov <sodrian@gmail.com>
2017-01-14 10:06:41 +0300

    editing kt28_binary_calculator.py

--- kt28_binary_calculator.py
+++ kt28_binary_calculator.py
@@ -1,3 +1,26 @@
+"""Title: Binary Calculator
+URL: https://www.codewars.com/kata/binary-calculator
+Description:
+
+In this kata you need to write a function that will receive two strings (```n1``` and ```n2```), each representing an integer as a binary number. A third parameter will be provided (```o```) as a string representing one of the following operators: add, subtract, multiply.
+
+Your task is to write the calculate function so that it will perform the arithmetic and the result returned should be a string representing the binary result.
+
+Examples:
+```
+1 + 1 === 10
+10 + 10 === 100
+```
+
+Negative binary numbers are usually preceded by several 1's. For this kata, negative numbers can be represented with the negative symbol at the beginning of the string.
+
+Examples of negatives:
+```
+1 - 10 === -1
+10 - 100 === -10
+```
+"""
+
 import re
 from operator import add, sub, mul
 BIN_PTRN = r'([-]{0,1})0b(\d+)'
