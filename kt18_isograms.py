61944270aac05b32c6cb2e11b9255c606bc3b61d
Dmitriy Berkutov <sodrian@gmail.com>
2017-01-14 10:06:41 +0300

    editing kt18_isograms.py

--- kt18_isograms.py
+++ kt18_isograms.py
@@ -1,2 +1,31 @@
+"""Title: Isograms
+URL: https://www.codewars.com/kata/isograms
+Description:
+
+An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+```haskell
+isIsogram "Dermatoglyphics" == true
+isIsogram "moose" == false
+isIsogram "aba" == false
+```
+```javascript
+isIsogram( "Dermatoglyphics" ) == true
+isIsogram( "aba" ) == false
+isIsogram( "moOse" ) == false // -- ignore letter case
+```
+```python
+is_isogram("Dermatoglyphics" ) == true
+is_isogram("aba" ) == false
+is_isogram("moOse" ) == false # -- ignore letter case
+```
+```ruby
+is_isogram("Dermatoglyphics" ) == true
+is_isogram("aba" ) == false
+is_isogram("moOse" ) == false # -- ignore letter case
+```
+
+"""
+
 def is_isogram(string):
     return len(set(string.lower())) == len(string.lower())
