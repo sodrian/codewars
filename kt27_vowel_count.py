718cf79762057e4a9f077f52c30812302c0233a1
Dmitriy Berkutov <sodrian@gmail.com>
2017-01-14 10:06:41 +0300

    editing kt27_vowel_count.py

--- kt27_vowel_count.py
+++ kt27_vowel_count.py
@@ -1,2 +1,11 @@
+"""Title: Vowel Count
+URL: https://www.codewars.com/kata/vowel-count
+Description:
+
+Return the number (count) of vowels in the given string. 
+
+We will consider a, e, i, o, and u as vowels for this Kata.
+"""
+
 def getCount(inputStr):
     return len([ch for ch in inputStr.lower() if ch in 'aeiou'])
