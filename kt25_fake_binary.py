5ed6947c42e97c4cf137b7da2e5629520af6a808
Dmitriy Berkutov <sodrian@gmail.com>
2017-01-14 10:06:41 +0300

    editing kt25_fake_binary.py

--- kt25_fake_binary.py
+++ kt25_fake_binary.py
@@ -1,2 +1,9 @@
+"""Title: Fake Binary
+URL: https://www.codewars.com/kata/fake-binary
+Description:
+
+Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+"""
+
 def fake_bin(x):
     return ''.join([str(int(d) // 5) for d in x]) 
