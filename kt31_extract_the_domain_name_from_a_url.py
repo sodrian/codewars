6428e08575000a39316d0c7e5f88117d14a710f3
Dmitriy Berkutov <sodrian@gmail.com>
2017-01-14 10:06:41 +0300

    editing kt31_extract_the_domain_name_from_a_url.py

--- kt31_extract_the_domain_name_from_a_url.py
+++ kt31_extract_the_domain_name_from_a_url.py
@@ -1,3 +1,25 @@
+"""Title: Extract the domain name from a URL
+URL: https://www.codewars.com/kata/extract-the-domain-name-from-a-url-1
+Description:
+
+Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+```ruby
+domain_name("http://github.com/carbonfive/raygun") == "github" 
+domain_name("http://www.zombie-bites.com") == "zombie-bites"
+domain_name("https://www.cnet.com") == "cnet"
+```
+```python
+domain_name("http://github.com/carbonfive/raygun") == "github" 
+domain_name("http://www.zombie-bites.com") == "zombie-bites"
+domain_name("https://www.cnet.com") == "cnet"
+```
+```javascript
+domainName("http://github.com/carbonfive/raygun") == "github" 
+domainName("http://www.zombie-bites.com") == "zombie-bites"
+domainName("https://www.cnet.com") == "cnet"
+```
+"""
+
 import re
 PTRN = r'^(https?://)?(www\.)?([a-z-]*)\..+$'
 
