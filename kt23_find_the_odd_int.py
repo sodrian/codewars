be73f59a9fcb15ca6a930827b30c80e9b1df63c0
Dmitriy Berkutov <sodrian@gmail.com>
2017-01-14 10:06:41 +0300

    editing kt23_find_the_odd_int.py

--- kt23_find_the_odd_int.py
+++ kt23_find_the_odd_int.py
@@ -1,3 +1,13 @@
+"""Title: Find the odd int
+URL: https://www.codewars.com/kata/find-the-odd-int
+Description:
+
+Given an array, find the int that appears an odd number of times.
+
+There will always be only one integer that appears an odd number of times.
+
+"""
+
 def find_it(seq):
     print(seq)
     d = dict()
