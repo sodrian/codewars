63e07e976027b49b0b83942b51d0f90506e4c74a
Dmitriy Berkutov <sodrian@gmail.com>
2017-02-06 22:46:48 +0000

    editing kt36_product_of_consecutive_fib_numbers.py

--- kt36_product_of_consecutive_fib_numbers.py
+++ kt36_product_of_consecutive_fib_numbers.py
@@ -26,6 +26,14 @@ F(m) being the smallest one such as `F(m) * F(m+1) > prod`.
 
 # Examples
 
+```rust
+productFib(714) # should return (21, 34, true), 
+                # since F(8) = 21, F(9) = 34 and 714 = 21 * 34
+
+productFib(800) # should return (34, 55, false), 
+                # since F(8) = 21, F(9) = 34, F(10) = 55 and 21 * 34 < 800 < 34 * 55
+```
+
 ```ruby
 productFib(714) # should return [21, 34, true], 
                 # since F(8) = 21, F(9) = 34 and 714 = 21 * 34
@@ -96,6 +104,13 @@ productFib(714) # should return {21, 34, true},
 productFib(800) # should return {34, 55, false}, 
                 # since F(8) = 21, F(9) = 34, F(10) = 55 and 21 * 34 < 800 < 34 * 55
 ```
+```c
+productFib(714) # should return {21, 34, true}, 
+                # since F(8) = 21, F(9) = 34 and 714 = 21 * 34
+
+productFib(800) # should return {34, 55, false}, 
+                # since F(8) = 21, F(9) = 34, F(10) = 55 and 21 * 34 < 800 < 34 * 55
+```
 ```haskell
 productFib 714 -- should return (21, 34, True)
                -- since F(8) = 21, F(9) = 34 and 714 = 21 * 34
@@ -103,7 +118,9 @@ productFib 800 -- should return (34, 55, False),
                -- since F(8) = 21, F(9) = 34, F(10) = 55 and 21 * 34 < 800 < 34 * 55
 ```
 
-**Note:** Not useful here but we can tell how to choose the number n up to which to go: we can use the "golden ratio" phi which is `(1 + sqrt(5))/2` knowing that F(n) is asymptotic to: `phi^n / sqrt(5)`. That gives a possible upper bound to n.
+**Notes:** Not useful here but we can tell how to choose the number n up to which to go: we can use the "golden ratio" phi which is `(1 + sqrt(5))/2` knowing that F(n) is asymptotic to: `phi^n / sqrt(5)`. That gives a possible upper bound to n.
+
+You can see examples in "Example test".
 
 ### References 
 
