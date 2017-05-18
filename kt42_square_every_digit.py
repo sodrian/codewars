8ff24e0799f278c7f36d720366894ee809c899eb
Dmitriy Berkutov <sodrian@gmail.com>
2017-05-18 15:41:59 +0000

    adding Square Every Digit

---
+++ kt42_square_every_digit.py
@@ -0,0 +1,14 @@
+"""Title: Square Every Digit
+URL: https://www.codewars.com/kata/square-every-digit
+Description:
+
+Welcome. In this kata, you are asked to square every digit of a number.
+
+For example, if we run 9119 through the function, 811181 will come out.
+
+**Note:** The function accepts an integer and returns an integer
+"""
+
+def square_digits(num):
+    num = [int(str(num)[i]) for i in range(len(str(num)))]
+    return int(''.join([str(n ** 2) for n in num]))
