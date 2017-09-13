136d83a3c5b46981a297728db2b397d4f3702f2e
Dmitriy Berkutov <sodrian@gmail.com>
2017-09-13 14:55:39 +0000

    editing kt34_moving_zeros_to_the_end.py

--- kt34_moving_zeros_to_the_end.py
+++ kt34_moving_zeros_to_the_end.py
@@ -13,6 +13,9 @@ move_zeros([false,1,0,1,2,0,1,3,"a"]) # returns[false,1,1,2,1,3,"a",0,0]
 ```coffeescript
 moveZeros [false,1,0,1,2,0,1,3,"a"] # returns[false,1,1,2,1,3,"a",0,0]
 ```
+```csharp
+Kata.MoveZeroes(new int[] {1, 2, 0, 1, 0, 1, 0, 3, 0, 1}) => new int[] {1, 2, 1, 1, 3, 1, 0, 0, 0, 0}
+```
 """
 
 def move_zeros(array):
