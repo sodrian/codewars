abfa77bdfc7ae03d29661067719d48dad74d4cbb
Dmitriy Berkutov <sodrian@gmail.com>
2017-09-13 14:55:39 +0000

    editing kt30_where_my_anagrams_at.py

--- kt30_where_my_anagrams_at.py
+++ kt30_where_my_anagrams_at.py
@@ -14,12 +14,54 @@ What is an anagram? Well, two words are anagrams of each other if they both cont
 
 Write a function that will find all the anagrams of a word from a list. You will be given two inputs a word and an array with words. You should return an array of all the anagrams or an empty array if there are none. For example:
 
-```
+```javascript
 anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
 
 anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) => ['carer', 'racer']
 
 anagrams('laser', ['lazing', 'lazy',  'lacer']) => []
+```
+```php
+anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']); // => ['aabb', 'bbaa']
+
+anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']); // => ['carer', 'racer']
+
+anagrams('laser', ['lazing', 'lazy',  'lacer']); // => []
+```
+```javascript
+anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
+
+anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) => ['carer', 'racer']
+
+anagrams('laser', ['lazing', 'lazy',  'lacer']) => []
+```
+```coffeescript
+anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
+
+anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) => ['carer', 'racer']
+
+anagrams('laser', ['lazing', 'lazy',  'lacer']) => []
+```
+```python
+anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
+
+anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) => ['carer', 'racer']
+
+anagrams('laser', ['lazing', 'lazy',  'lacer']) => []
+```
+```csharp
+Kata.Anagrams("abba", new List<string> {"aabb", "abcd", "bbaa", "dada"}) => new List<string> {"aabb", "bbaa"}
+
+Kata.Anagrams("racer", new List<string> {"crazer", "carer", "racar", "caers", "racer"}) => new List<string> {"carer", "racer"}
+
+Kata.Anagrams("laser", new List<string> {"lazing", "lazy", "lacer"}) => new List<string>()
+```
+```r
+anagrams("abba", c("aabb", "abcd", "bbaa", "dada")) => c("aabb", "bbaa")
+
+anagrams("racer", c("crazer", "carer", "racar", "caers", "racer")) => c("carer", "racer")
+
+anagrams("laser", c("lazing", "lazy", "lacer")) => character(0) # no anagram, return empty vector
 ```
 """
 
