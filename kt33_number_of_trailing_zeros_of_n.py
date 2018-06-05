95b6dfe4dac6aac40676b5bc66267a82b83e1ff9
Dmitriy Berkutov <sodrian@gmail.com>
2018-06-05 08:16:00 +0000

    editing kt33_number_of_trailing_zeros_of_n.py

--- kt33_number_of_trailing_zeros_of_n.py
+++ kt33_number_of_trailing_zeros_of_n.py
@@ -4,18 +4,23 @@ Description:
 
 Write a program that will calculate the number of trailing zeros in a factorial of a given number.
 
-http://mathworld.wolfram.com/Factorial.html 
+`N! = 1 * 2 * 3 *  ... * N`
 
-N! = 1 \* 2 \* 3 \* 4 ... N
+Be careful `1000!` has 2568 digits...
 
+For more info, see: http://mathworld.wolfram.com/Factorial.html 
+
+## Examples
 
 ```
-zeros(12) = 2 # 1 * 2 * 3 .. 12 = 479001600 
-that has 2 trailing zeros 4790016(00)
+zeros(6) = 1
+# 6! = 1 * 2 * 3 * 4 * 5 * 6 = 720 --> 1 trailing zero
+
+zeros(12) = 2
+# 12! = 479001600 --> 2 trailing zeros
 ```
 
-
-Be careful 1000! has length of 2568 digital numbers.
+*Hint: You're not meant to calculate the factorial. Find another way to find the number of zeros.*
 """
 
 def zeros(n):
