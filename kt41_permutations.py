2f669cb660e6d3aa8cc11fa6b8defc5382b834b4
Dmitriy Berkutov <sodrian@gmail.com>
2018-06-05 08:16:04 +0000

    editing kt41_permutations.py

--- kt41_permutations.py
+++ kt41_permutations.py
@@ -11,6 +11,11 @@ permutations('a'); // ['a']
 permutations('ab'); // ['ab', 'ba']
 permutations('aabb'); // ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']
 ```
+```cpp
+permutations("a"); // => vector<string> {"a"}
+permutations("ab"); // => vector<string> {"ab", "ba"}
+permutations("aabb"); // => vector<string> {"aabb", "abab", "abba", "baab", "baba", "bbaa"}
+```
 ```ruby
 permutations('a'); # ['a']
 permutations('ab'); # ['ab', 'ba']
