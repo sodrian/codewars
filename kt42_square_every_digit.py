e752dacec02148977cf3e674e430759c4b5c8030
Dmitriy Berkutov <sodrian@gmail.com>
2018-06-05 08:16:06 +0000

    editing kt42_square_every_digit.py

--- kt42_square_every_digit.py
+++ kt42_square_every_digit.py
@@ -4,7 +4,7 @@ Description:
 
 Welcome. In this kata, you are asked to square every digit of a number.
 
-For example, if we run 9119 through the function, 811181 will come out.
+For example, if we run 9119 through the function, 811181 will come out, because 9<sup>2</sup> is 81 and 1<sup>2</sup> is 1.
 
 **Note:** The function accepts an integer and returns an integer
 """
